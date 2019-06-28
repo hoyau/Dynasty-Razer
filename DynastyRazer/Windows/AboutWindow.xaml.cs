399249e36d942a7cb501c9c731e6f09b93cb4c54
Justin <justin.anderwert@hotmail.com>
2019-06-28 22:12:59 +0200

    moved window to other folder and dry icon

--- DynastyRazer/Windows/AboutWindow.xaml.cs
+++ DynastyRazer/Windows/AboutWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace DynastyRazer
+namespace DynastyRazer.Windows
 {
     /// <summary>
     /// Interaction logic for AboutWindow.xaml
--- DynastyRazer/Windows/MainWindow.xaml.cs
+++ DynastyRazer/Windows/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DynastyRazer
+namespace DynastyRazer.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
